12dcc74840db5a722fd7756042980243902e880f
vihan lakshitha <Vihanlakshitha27@gmail.com>
2026-08-16 10:55:13 +0530

    Upadate API to v2.0.0

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    status: 'success',
-    message: 'Welcome to my Micro API Service on GCP!',
-    version: '1.0.0',
-    deployedBy: 'Vihan - DevOps Engineer'
+    status: "success",
+    message: "Welcome to my Micro API Service on GCP!",
+    version: "1.0.0",
+    deployedBy: "Vihan - DevOps Engineer"
   });
 });
 
-app.listen(PORT, () => {
-  console.log(Server running on port );
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
